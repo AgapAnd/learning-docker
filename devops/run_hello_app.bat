fa4e38f0a9a17a4229bdc10a99d45694f42d4d50
AgapAnd <braniganzepp@gmail.com>
2024-10-25 16:19:13 +0300

    Update run_hello_app.bat

--- devops/run_hello_app.bat
+++ devops/run_hello_app.bat
@@ -2,7 +2,7 @@
 cls
 echo "Hello World" application
 echo.
-echo Step 1. Checking necessary conditions for running app...
+echo Step 1. Checking necessary conditions for compiling and running app...
 
 if not exist ../application/src/ru/agapov/app/Hello.java (
 	echo File "Hello.java" not found
@@ -11,10 +11,14 @@ if not exist ../application/src/ru/agapov/app/Hello.java (
 echo.
 
 
-echo Step 2. Running application...
+echo Step 2. Compiling application...
 echo.
 
 docker run -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /usr/local/openjdk-8/bin/javac -d /mnt/application /mnt/application/src/ru/agapov/app/Hello.java
+
+
+echo Step 3. Running application...
+echo.
 
 docker run -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /usr/local/openjdk-8/bin/java -classpath /mnt/application ru.agapov.app.Hello
 echo.
