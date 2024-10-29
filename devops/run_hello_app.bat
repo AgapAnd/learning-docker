377862676b8587efa6dc48bba4849a864eb09d8e
AgapAnd <braniganzepp@gmail.com>
2024-10-29 17:03:07 +0300

    Update run_hello_app.bat

--- devops/run_hello_app.bat
+++ devops/run_hello_app.bat
@@ -3,52 +3,56 @@ cls
 echo "Hello World" application
 echo.
 
+
 if not exist logs (
 	md logs
 )
 
-set log_file="logs/log.txt"
+set log_file=logs/hello_app_log.log
 
-> %log_file% (
-echo Date: %date%, time: %time%
+echo [%date%, %time:~,-3%] INFO "Hello World" application logs > %log_file%
+
+
+if not exist ../application/bin (
+	cd ../application
+	md bin
+	cd ../devops
+	echo [%date%, %time:~,-3%] INFO A new folder "/application/bin" was made >> %log_file%
 )
 
 
 echo Step 1. Checking necessary conditions for compiling and running app...
 echo.
-echo 	Step 1. Checking files >> %log_file%
+echo [%date%, %time:~,-3%] INFO Step 1. Checking files >> %log_file%
 
 if not exist ../application/src/ru/agapov/app/Hello.java (
 	echo File "Hello.java" was not found
-	echo [ERROR] File "Hello.java" was not found >> %log_file%
+	echo [%date%, %time:~,-3%] ERROR File "Hello.java" was not found >> %log_file%
 	goto end
 )
-echo [INFO] "Hello.java" - OK >> %log_file%
+echo [%date%, %time:~,-3%] INFO "Hello.java" - OK >> %log_file%
 
 if exist ../application/bin/ru/agapov/app/Hello.class (
-	echo [INFO] "Hello.class" - OK, compilation is not needed >> %log_file%
+	echo [%date%, %time:~,-3%] INFO "Hello.class" - OK, compilation is not needed >> %log_file%
 	goto running
 )
 
 
 echo Step 2. Compiling application...
 echo.
-echo 	Step 2. Compiling application >> %log_file%
-
-docker run --rm -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /bin/mkdir /mnt/application/bin
-echo [INFO] A new folder "/bin" was made >> %log_file%
+echo [%date%, %time:~,-3%] INFO Step 2. Compiling application >> %log_file%
 
 docker run --rm -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /usr/local/openjdk-8/bin/javac -d /mnt/application/bin /mnt/application/src/ru/agapov/app/Hello.java
-echo [INFO] File Hello.class was compiled >> %log_file%
+echo [%date%, %time:~,-3%] INFO File "Hello.class" was compiled >> %log_file%
 
 
 : running
 echo Step 3. Running application...
 echo.
-echo 	Step 3. Running application >> %log_file%
+echo [%date%, %time:~,-3%] INFO Step 3. Running application >> %log_file%
 
 docker run --rm -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /usr/local/openjdk-8/bin/java -classpath /mnt/application/bin ru.agapov.app.Hello
-echo [INFO] Application was successfully runned >> %log_file%
+echo [%date%, %time:~,-3%] INFO Application was successfully runned >> %log_file%
 echo.
 
 
