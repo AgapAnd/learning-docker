09a7423d0d24c313e7c7757d149028f0f4e713aa
AgapAnd <braniganzepp@gmail.com>
2024-11-05 10:20:39 +0300

    Update run_hello_app.bat

--- devops/run_hello_app.bat
+++ devops/run_hello_app.bat
@@ -8,34 +8,31 @@ if not exist logs (
 	md logs
 )
 
-set log_file=logs/hello_app_log.log
+set log_file=logs/hello_app.log
 
-echo [%date%, %time:~,-3%] INFO "Hello World" application logs > %log_file%
+rem: > %log_file%
 
 
-if not exist ../application/bin (
-	cd ../application
-	md bin
-	cd ../devops
-	echo [%date%, %time:~,-3%] INFO A new folder "/application/bin" was made >> %log_file%
+cd ../application
+if exist bin (
+	rd bin /s /q
+	echo [%date%, %time:~,-3%] INFO A folder "application/bin" was deleted >> ../devops/%log_file%
 )
+md bin
+cd ../devops
+echo [%date%, %time:~,-3%] INFO A new folder "application/bin" was created >> %log_file%
 
 
 echo Step 1. Checking necessary conditions for compiling and running app...
 echo.
-echo [%date%, %time:~,-3%] INFO Step 1. Checking files >> %log_file%
+echo [%date%, %time:~,-3%] INFO Step 1. Checking necessary conditions for compiling and running application >> %log_file%
 
 if not exist ../application/src/ru/agapov/app/Hello.java (
 	echo File "Hello.java" was not found
 	echo [%date%, %time:~,-3%] ERROR File "Hello.java" was not found >> %log_file%
 	goto end
 )
-echo [%date%, %time:~,-3%] INFO "Hello.java" - OK >> %log_file%
-
-if exist ../application/bin/ru/agapov/app/Hello.class (
-	echo [%date%, %time:~,-3%] INFO "Hello.class" - OK, compilation is not needed >> %log_file%
-	goto running
-)
+echo [%date%, %time:~,-3%] INFO File "Hello.java" - exists >> %log_file%
 
 
 echo Step 2. Compiling application...
@@ -46,7 +43,6 @@ docker run --rm -it -v "%cd%/..:/mnt" maven:3.6.3-openjdk-8-slim /usr/local/open
 echo [%date%, %time:~,-3%] INFO File "Hello.class" was compiled >> %log_file%
 
 
-: running
 echo Step 3. Running application...
 echo.
 echo [%date%, %time:~,-3%] INFO Step 3. Running application >> %log_file%
